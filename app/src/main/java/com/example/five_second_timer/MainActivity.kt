c6a253859cf8643d54209768c5b7bc536ba2dede
syuheifujita <mcslr772ed@gmail.com>
2019-11-30 15:08:55 +0900

    #3 Handlerをインスタンス化

--- app/src/main/java/com/example/five_second_timer/MainActivity.kt
+++ app/src/main/java/com/example/five_second_timer/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.five_second_timer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val handler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
