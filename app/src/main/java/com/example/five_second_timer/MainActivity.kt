9ba0e3d1d24ae809d4ca92577ede730899be6ddf
syuheifujita <mcslr772ed@gmail.com>
2019-11-30 13:55:07 +0900

    first commit

---
+++ app/src/main/java/com/example/five_second_timer/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.five_second_timer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
